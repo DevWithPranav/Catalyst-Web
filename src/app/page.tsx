a5a466fcddd9b5102b950ae25d29b500545b54e6
Abin S Manoj <104203287+Flash500x@users.noreply.github.com>
2025-12-30 21:25:40 +0530

    Reapply "updated version"
    This reverts commit 38ffeb03ad9bb442ea377750ad8280eca4239694.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,41 @@
 import React from "react";
 import HorizontalScroll from "../components/scroll";
 import Card from "../components/card";
+import TypingText from "@/components/ui/shadcn-io/typing-text";
+import { RippleButton } from "@/components/ui/shadcn-io/ripple-button";
+import { ColourfulText } from "@/components/ui/shadcn-io/colourful-text";
+import HorizontalScrollCards from "../components/horizontalScroll";
+import ContactCard from "../components/contactCard";
+import ArrowButton from "../components/button";
 const page = () => {
   return (
     <div>
+      <section className="flex flex-col items-center text-center justify-center  h-[85vh]">
+        <img
+          src="/hero_logo.png"
+          alt="catalyst logo"
+          className="w-60 h-60 mb-5"
+        />
+        <h1 className="text-3xl font-bold font-primary">CATALYST</h1>
+        <h2 className="text-3xl font-bold font-primary">
+          MAR BASELIOS <br></br>IEDC
+        </h2>
+      </section>
+
       <section className="flex items-center  text-center justify-center ">
-        <div className="flex flex-col items-center gap-5 my-44">
-          <img src="/hero_logo.png" alt="" className="w-45 h-40" />
-          <h1 className="text-3xl font-bold font-primary">
-            CATALYST
-            <br /> MAR BASELIOS IEDC
-          </h1>
+        <div className="flex flex-col items-center gap-5 text-center">
+          <p className="text-xl font-secondary">Building</p>
+          <div className="flex flex-wrap items-center  text-2xl font-bold font-primary text-center justify-center mx-10">
+            <h1 className="text-center">
+              A LEGACY OF INNOVATION AND ENTREPRENEURSHIP
+            </h1>
+          </div>
         </div>
       </section>
 
-      <section className="flex items-center  text-center justify-center h-screen">
-        <div className="flex flex-col items-center gap-5">
-          <p>Building</p>
-          <h1 className="text-2xl font-bold font-primary">
-            A LEGACY OF INNOVATION AND ENTREPRENEURSHIP
-          </h1>
-        </div>
-      </section>
-
-      <section className=" px-8 mb-40">
+      <section className=" px-8 mb-40 mt-52">
         <h3 className="text-2xl font-bold font-primary text-left ">ABOUT US</h3>
-        <p className="text-left">
+        <p className="text-left ">
           The Innovation and Entrepreneurship Development Centre of Mar Baselios
           College of Engineering and Technology, Catalyst was inaugurated in the
           year 2013 with a purpose of inspiring students to become independent
@@ -46,13 +56,59 @@ const page = () => {
         <HorizontalScroll />
       </section>
 
-      <section className="mt-40 mx-7">
+      <section className="mt-28 mx-7">
         <h2 className="text-2xl font-primary text-center mb-7">
           CATALYST MATRIX
         </h2>
         <Card />
       </section>
-      
+
+      <section className="mt-28 mx-7">
+        <h2 className="text-2xl font-primary text-left mb-4 ">THE EVENTS</h2>
+        <p className="text-left">
+          Catalyst events is a platform for students to showcase their ideas and
+          projects{" "}
+        </p>
+        <HorizontalScrollCards />
+        <div className="mt-7">
+          <ArrowButton label="Events" />
+        </div>
+      </section>
+
+      <section className="mt-28 mx-7">
+        <h2 className="text-2xl font-primary text-left mb-4 ">THE PIONEERS</h2>
+        <p className="text-left">
+          Catalyst is a hub of innovation and entrepreneurship, where students
+          are encouraged to explore their ideas and turn them into reality.
+        </p>
+        <HorizontalScrollCards />
+      </section>
+
+      <section className="flex items-center  text-center justify-center mt-32 mb-32 ">
+        <div className="flex flex-col items-center gap-5 text-center">
+          <p className="text-xl font-secondary">Your life's best choice</p>
+          <div className="flex flex-wrap items-center  text-2xl font-bold font-primary text-center justify-center mx-10">
+            <h1 className="text-center">
+              MORE THAN JUST A CENTRE, WE ARE A FAMILY
+            </h1>
+          </div>
+        </div>
+      </section>
+
+      <section className="mt-28 mx-7">
+        <h2 className="text-2xl font-primary mb-7">OUR TEAM</h2>
+        <p className="text-left">
+          Catalyst events is a platform for students to showcase their ideas and
+          projects{" "}
+        </p>
+        <div className="mt-7">
+          <ArrowButton label="Execom" />
+        </div>
+      </section>
+
+      <section className="mt-28 mx-7">
+        <ContactCard />
+      </section>
     </div>
   );
 };
